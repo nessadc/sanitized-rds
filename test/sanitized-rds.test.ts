4a69aff1be3652ee6f28eb23b759c8ce5ed2825d
Nessad Choudhury <nessadchoudhury@gmail.com>
2023-08-26 10:21:43 -0400

    Initial commit

---
+++ test/sanitized-rds.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SanitizedRds from '../lib/sanitized-rds-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sanitized-rds-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SanitizedRds.SanitizedRdsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
